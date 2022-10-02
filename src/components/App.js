91fe8a5dfe6bf60053466789841e88c796824d72
Stefan <84741877+StefanLambic@users.noreply.github.com>
2022-10-03 00:00:07 +0200

    Removed Unnecesary Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,11 @@
 import Launches from "./Launches";
-import Error from "./Error";
 import Nav from "./Nav";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
-    <BrowserRouter>
+    <>
     <Nav/>
-    <Routes>
-    <Route path = "/" element ={<Launches/>}/>
-    <Route path='*' element={<Error />} />
-    </Routes>
-    </BrowserRouter>
+    <Launches/>
+    </>
     
   )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faRocket} from "@fortawesome/free-solid-svg-icons";
-import { Link } from 'react-router-dom';
 import "../styling/nav.css";
 const Nav = () => {
   return (
     <nav className="nav">
       <FontAwesomeIcon icon={faRocket} className="icon" />
-      <Link className="logo" to ='/'> eSpace 
-      </Link>
+      <a className="logo" to ='/'> eSpace 
+      </a>
     </nav>
   )
 }
